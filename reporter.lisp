03c0d8fb9fa3b042a74df21b6fdfa2fa7d6bedf4
cxxxr <g23tlm@gmail.com>
2020-02-22 14:45:54 +0900

    add reporter/registry

--- reporter.lisp
+++ reporter.lisp
@@ -4,6 +4,8 @@
   (:import-from #:rove/core/stats
                 #:stats
                 #:*stats*)
+  (:import-from #:rove/reporter/registry
+                #:get-reporter)
   (:import-from #:bordeaux-threads)
   (:export #:reporter
            #:reporter-stream
@@ -20,18 +22,21 @@
            :accessor reporter-stream)))
 
 (defun make-reporter (style &key (stream *report-stream*))
-  (let* ((package-name
-           (format nil "~A/~A"
-                   '#:rove/reporter
-                   style))
-         (package (find-package package-name)))
-    (unless package
-      #+quicklisp (ql:quickload (string-downcase package-name) :silent t)
-      #-quicklisp (asdf:load-system (string-downcase package-name))
-      (setf package (find-package package-name)))
-    (make-instance
-     (intern (format nil "~A-~A" style '#:reporter) package)
-     :stream stream)))
+  (let ((class-name (get-reporter style)))
+    (if class-name
+        (make-instance class-name :stream stream)
+        (let* ((package-name
+                 (format nil "~A/~A"
+                         '#:rove/reporter
+                         style))
+               (package (find-package package-name)))
+          (unless package
+            #+quicklisp (ql:quickload (string-downcase package-name) :silent t)
+            #-quicklisp (asdf:load-system (string-downcase package-name))
+            (setf package (find-package package-name)))
+          (make-instance
+           (intern (format nil "~A-~A" style '#:reporter) package)
+           :stream stream)))))
 
 (defgeneric print-message (reporter desc)
   (:method ((reporter reporter) desc)
