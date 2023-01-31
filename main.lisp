2aaa247512583dc69ae85bfeb46c3f08ad523aac
cxxxr <g23tlm@gmail.com>
2023-01-31 19:45:54 +0900

    extract function

--- main.lisp
+++ main.lisp
@@ -53,10 +53,14 @@
 (defgeneric run (target &key style env)
   (:documentation "Run a test package."))
 
-(defmethod run-test (test-name &key (style :spec))
+(defun ensure-test (test-name)
   (let ((test (get-test test-name)))
     (unless test
       (error "No test found for ~S" test-name))
+    test))
+
+(defmethod run-test (test-name &key (style :spec))
+  (let ((test (ensure-test test-name)))
     (with-reporter style
       (testing nil
                (funcall test)))))
